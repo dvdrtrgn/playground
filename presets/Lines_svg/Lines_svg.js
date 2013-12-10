d46cb02d917624ce87a5cd59aa4a90e027fe4319
dvdrtrgn <dvdrtrgn@gmail.com>
2013-12-10 04:45:16 -0600

    tighten

--- presets/Lines_svg/Lines_svg.js
+++ presets/Lines_svg/Lines_svg.js
@@ -1,85 +1,66 @@
 /*jslint es5:true, white:false */
 /*globals Util, $data, d3 */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-var C = console, W = window;
+var C = console,
+    D = {},
+    W = window;
 
-W.svg = Util.createSVG('0 0 200 200');
+W.D = D;
+
 function midpoint(ele) {
     var w, h;
     w = parseInt(ele.style('width')) / 2;
     h = parseInt(ele.style('height')) / 2;
     return [w, h];
 }
-
 function reCalc(arr) {
-    return [arr[0] - W.mid[0], arr[1] - W.mid[1]];
+    return [arr[0] - D.mid[0], arr[1] - D.mid[1]];
 }
-
-function draw() {
-    W.path = svg.append('path') //
-    .attr('class', 't2') //
-    .attr('d', W.calc($data));
+function draw(cls) {
+    cls = cls || 't2';
+    D.path = D.svg.append('path') //
+    .attr('class', cls) //
+    .attr('d', D.calc($data));
 }
-
-// LINE 1
-W.svg.append('path').attr('class', 't1') //
-.attr('d', 'M0,60 L50,110 L90,70 L140,100');
-
-// LINE 2
-W.calc = d3.svg.line();
-W.mid = midpoint(W.svg);
-W.arr = [0,0];
-W.foo = '';
-
-
-W.add = function (arr) {
-    if (arr) {
-        $data.push(arr);
-        W.arr = arr;
+// append a new line path
+D.app = function (arr1, arr2, cls) {
+    cls = cls || 't1';
+    if (arr1 && arr2) {
+        $data = [arr1, arr2];
     }
-    draw();
-}
-
-W.app = function (arr) {
-    W.foo = W.foo || W.path.attr('d');
+    draw(cls);
+};
+// add points to last path
+D.add = function (arr) {
+    D.dstr = (D.dstr || D.path.attr('d'));
     if (arr) {
         arr = reCalc(arr);
-        W.foo = W.foo + 'L' + arr;
-        W.path.attr('d', W.foo + 'z');
+        D.dstr += ('L' + arr);
+        D.path.attr('d', D.dstr);
     }
-}
+};
+D.adds = function () {
+    var c = d3.event.sourceEvent;
+    D.add([c.x, c.y]);
+    D.path.attr('class', 't2') //
+};
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-W.svg.call(d3.behavior.drag() //
-    .on('dragstart', function () {
-        var c = d3.event.sourceEvent;
-        W.app([c.x, c.y]);
-        console.log(c);
-    })
-    .on('drag', function () {
-        var c = d3.event;
-        W.app([c.x, c.y]);
-    })
-);
+D.svg = Util.createSVG('-100 -100 200 200');
+
+D.calc = d3.svg.line();
+D.mid = midpoint(D.svg);
+D.arr = [0,0];
+D.dstr = '';
+
+D.svg.call(d3.behavior.drag().on('dragstart', D.adds));
 
 function drawAxes() {
-    W.add([   0,-100]);
-    W.add([   0, 100]);
-    W.add([-100,   0]);
-    W.add([ 100,   0]);
+    D.app([0,-100], [0,100]);
+    D.app([-100,0], [100,0]);
+    D.app([0,0], [0,0]);
 }
 
 drawAxes();
 
-//
-//
-//
-// line = d3.svg.line().x(x).y(y);
-//
-// path = svg //
-// .selectAll('path') //
-// .data($data) //
-// .enter() //
-// .append('path') //
-// .attr('d', line);
-//
-// take any coordinate and move/draw to the point
+console.log(D);
