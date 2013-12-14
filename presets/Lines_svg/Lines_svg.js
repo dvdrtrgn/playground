c49fe11121170b0ca3a97c3329b7420ab2a1bf73
dvdrtrgn <dvdrtrgn@gmail.com>
2013-12-14 10:26:51 -0600

    tidy overload

--- presets/Lines_svg/Lines_svg.js
+++ presets/Lines_svg/Lines_svg.js
@@ -1,20 +1,21 @@
 /*jslint es5:true, white:false */
-/*globals Util, $data, d3 */
+/*globals Util, $data, d3, window */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-var C = console,
+var C = window.console,
     D = {
-        factor: 1.25,
-        margin: 1.5,
-        offset: -300,
-        ratio: 3,
-        size: null,
-        unit: 100,
+    data: null,
+    factor: 1.25,
+    margin: 1.5,
+    offset: - 300,
+    ratio: 3,
+    size: null,
+    unit: 100,
+    path: null,
+    last: {
         path: null,
-        last: {
-            path: null,
-            point: null,
-        },
+        point: null,
     },
+},
     W = window;
 
 W.D = D;
@@ -23,28 +24,40 @@ D.size = D.unit * D.ratio;
 function reCalc(arr) {
     return [arr[0] - D.mid[0], arr[1] - D.mid[1]];
 }
-function draw(cls) {
+
+function draw(pairs, cls) {
     cls = cls || 't2';
-    D.path = D.svg.append('path') //
+    D.path = D.svg.append('path') // new line
     .attr('class', cls) //
-    .attr('d', D.calc($data));
+    .attr('d', D.lnfn(pairs));
 }
+
 function store(lines, point) {
     D.last.lines = lines;
     D.last.point = point;
 }
 // append a new line path
 D.app = function (arr1, arr2, cls) {
-    cls = cls || 't1';
-    if (arr1 && arr2) {
-        $data = [arr1, arr2];
+
+    if (typeof arr2 !== 'object') {
+        cls = arr2;
+        arr2 = arr1;
+        arr1 = D.last.point || [0, 0];
     }
-    draw(cls);
+
+    cls = cls || 't1';
+
+    if (arr1 && arr2) {
+        D.data = [arr1, arr2];
+    }
+
+    draw(D.data, cls);
     store(D.path, arr2);
 };
 // add points to last path
-D.add = function (arr) {
+D.addstr = function (arr) {
     D.dstr = (D.dstr || D.path.attr('d'));
+
     if (arr) {
         arr = reCalc(arr);
         D.dstr += ('L' + arr);
@@ -54,35 +67,38 @@ D.add = function (arr) {
 D.add_h = function () {
     var c = d3.event.sourceEvent;
     // C.debug(c);
-    D.add([c.layerX - D.offset, c.layerY]);
-    D.path.attr('class', 't2') //
+    D.addstr([c.layerX - D.offset, c.layerY]);
+    D.path.attr('class', 't2');
 };
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 D.svg = Util.createSVG('-150 -150 300 300');
 
-D.calc = d3.svg.line();
+D.lnfn = d3.svg.line();
 D.mid = Util.midpoint(D.svg);
-D.arr = [0,0];
+D.arr = [0, 0];
 D.dstr = '';
 
 D.svg.call(d3.behavior.drag().on('dragstart', D.add_h));
 
 D.svg = D.svg.append('g');
-D.svg.attr('transform','scale(1.00) translate(' + D.offset + ', 0)');
+D.svg.attr('transform', 'scale(1.00) translate(' + D.offset + ', 0)');
 
 function drawAxes() {
     var F = D.ratio,
         U = D.unit;
-    D.app([   0 , -U],[   0 ,  U]);
-    D.app([  -U ,  0],[   U ,  0]);
-    D.app([-F*U , -U],[+F*U , -U]);
-    D.app([+F*U , -U],[+F*U , +U]);
-    D.app([+F*U , +U],[-F*U , +U]);
-    D.app([-F*U , +U],[-F*U , -U]);
-    D.app([   0 ,  0],[  10 , 10]);
+    // center
+    D.app([0, - U], [0, U]);
+    D.app([-U, 0], [U, 0]);
+    // bounds
+    D.app([-F * U, - U], [+F * U, - U]);
+    D.app([+F * U, + U]);
+    D.app([-F * U, + U]);
+    D.app([-F * U, - U]);
 }
 
 drawAxes();
 
-console.log(D);
+C.log(D);
+// etc
+D.app([9, 9], [33, 33]);
